e7b7a22b31f0ae558b20a99e9579fdb7fa3d458a
Zullybur <mattcasiro@gmail.com>
2019-11-23 22:03:13 -0800

    Add backups folder to reset list

--- runner.sh
+++ runner.sh
@@ -1,7 +1,7 @@
 reset_server()
 {
     echo "Resetting server..."
-    rm -rf world/ logs/ crash-reports/ l_m_modded_*/
+    rm -rf world/ logs/ crash-reports/ l_m_modded_*/ backups/
 }
 
 stage_mods()
